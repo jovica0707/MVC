9e98ad6750a34e610f55e92a4a750e723c031223
jovica0707 <58665696+jovica0707@users.noreply.github.com>
2020-09-08 17:21:26 +0200

    Add files via upload

---
+++ HomeWork3/SEDC.PizzaApp/SEDC.PizzaApp/Models/Domain/Pizza.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.Domain
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        //1. Add property IsOnPromotion to the Pizza Model and the PizzaViewModel. Use an if expression in the Pizza Details View 
+        //and add a h4 tag that will stay whether the pizza is on promotion or not depending on the property IsOnPromotion.
+
+        public bool IsOnPromotion { get; set; }
+    }
+}
---
+++ HomeWork3/SEDC.PizzaApp/SEDC.PizzaApp/Models/ViewModels/PizzaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Models.ViewModels
+{
+    //1. Add property IsOnPromotion to the Pizza Model and the PizzaViewModel. Use an if expression in the Pizza Details View 
+    //and add a h4 tag that will stay whether the pizza is on promotion or not depending on the property IsOnPromotion.
+    public class PizzaViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public bool IsOnPromotion { get; set; }
+    }
+}
